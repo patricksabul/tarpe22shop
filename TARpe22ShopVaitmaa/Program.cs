14c45d102ac561722cde0ca1697002b14ee9bfba
Estlib <75094683+Estlib@users.noreply.github.com>
2023-10-16 11:14:58 +0300

    database migration

--- TARpe22ShopVaitmaa/Program.cs
+++ TARpe22ShopVaitmaa/Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using TARpe22ShopVaitmaa.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<TARpe22ShopVaitmaaContext>(OptionsBuilderConfigurationExtensions => OptionsBuilderConfigurationExtensions.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
