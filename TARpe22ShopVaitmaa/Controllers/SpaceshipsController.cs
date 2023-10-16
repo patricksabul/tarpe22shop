599801222364a4a3b5114533459ef50ea7785237
Estlib <75094683+Estlib@users.noreply.github.com>
2023-10-16 11:38:54 +0300

    added index view wwith spaceships controller

---
+++ TARpe22ShopVaitmaa/Controllers/SpaceshipsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TARpe22ShopVaitmaa.Data;
+using TARpe22ShopVaitmaa.Models.Spaceship;
+
+namespace TARpe22ShopVaitmaa.Controllers
+{
+    public class SpaceshipsController : Controller
+    {
+        private readonly TARpe22ShopVaitmaaContext _context;
+        public SpaceshipsController(TARpe22ShopVaitmaaContext context) { _context = context; }
+        public IActionResult Index()
+        {
+            var result = _context.Spaceships
+                .OrderByDescending(y => y.CreatedAt)
+                .Select(x => new SpaceshipIndexViewModel { 
+                    Id = x.Id, 
+                    Name = x.Name, 
+                    Type = x.Type, 
+                    PassengerCount = x.PassengerCount, 
+                    EnginePower = x.EnginePower });
+            return View(result);
+        }
+    }
+}
