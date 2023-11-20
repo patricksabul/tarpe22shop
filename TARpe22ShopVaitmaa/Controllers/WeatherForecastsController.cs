09cf721908b4df5785c31311cec4c9f5b52d67de
Jüri Vaitmaa <jyri.vaitmaa@tthk.ee>
2023-11-20 10:21:34 +0200

    rewrote city view, and changed controller

--- TARpe22ShopVaitmaa/Controllers/WeatherForecastsController.cs
+++ TARpe22ShopVaitmaa/Controllers/WeatherForecastsController.cs
@@ -30,41 +30,39 @@ namespace TARpe22ShopVaitmaa.Controllers
         public IActionResult City()
         {
             WeatherResultDto dto = new();
-            WeatherViewModel vm = new();
 
             _weatherForecastsServices.WeatherDetail(dto);
 
-            vm.EffectiveDate = dto.EffectiveDate;
-            vm.EffectiveEpochDate = dto.EffectiveEpochDate;
+            WeatherViewModel vm = new();
+
+            vm.Date = dto.EffectiveDate;
+            vm.EpochDate = dto.EffectiveEpochDate;
             vm.Severity = dto.Severity;
             vm.Text = dto.Text;
             vm.Category = dto.Category;
-            vm.EndDate = dto.EndDate;
-            vm.EndEpochDate = dto.EndEpochDate;
             vm.MobileLink = dto.MobileLink;
             vm.Link = dto.Link;
-            vm.Date = dto.DailyForecastsDay;
-            vm.EpochDate = dto.DailyForecastsEpochDate;
+            vm.Category = dto.Category;
             
-            vm.Temperature.Minimum.Value = dto.TempMinValue;
-            vm.Temperature.Minimum.Unit = dto.TempMinUnit;
-            vm.Temperature.Minimum.UnitType = dto.TempMinUnitType;
+            vm.TempMinValue = dto.TempMinValue;
+            vm.TempMinUnit = dto.TempMinUnit;
+            vm.TempMinUnitType = dto.TempMinUnitType;
             
-            vm.Temperature.Maximum.Value = dto.TempMaxValue;
-            vm.Temperature.Maximum.Unit = dto.TempMaxUnit;
-            vm.Temperature.Maximum.UnitType = dto.TempMaxUnitType;
+            vm.TempMaxValue = dto.TempMaxValue;
+            vm.TempMaxUnit = dto.TempMaxUnit;
+            vm.TempMaxUnitType = dto.TempMaxUnitType;
 
-            vm.Day.Icon = dto.DayIcon;
-            vm.Day.IconPhrase = dto.DayIconPhrase;
-            vm.Day.HasPrecipitation = dto.DayHasPrecipitation;
-            vm.Day.PrecipitationType = dto.DayPrecipitationType;
-            vm.Day.PrecipitationIntensity = dto.DayPrecipitationIntensity;
+            vm.DayIcon = dto.DayIcon;
+            vm.DayIconPhrase = dto.DayIconPhrase;
+            vm.DayHasPrecipitation = dto.DayHasPrecipitation;
+            vm.DayPrecipitationType = dto.DayPrecipitationType;
+            vm.DayPrecipitationIntensity = dto.DayPrecipitationIntensity;
             
-            vm.Night.Icon = dto.NightIcon;
-            vm.Night.IconPhrase = dto.NightIconPhrase;
-            vm.Night.HasPrecipitation = dto.NightHasPrecipitation;
-            vm.Night.PrecipitationType = dto.NightPrecipitationType;
-            vm.Night.PrecipitationIntensity = dto.NightPrecipitationIntensity;
+            vm.NightIcon = dto.NightIcon;
+            vm.NightIconPhrase = dto.NightIconPhrase;
+            vm.NightHasPrecipitation = dto.NightHasPrecipitation;
+            vm.NightPrecipitationType = dto.NightPrecipitationType;
+            vm.NightPrecipitationIntensity = dto.NightPrecipitationIntensity;
 
             return View(vm);
         }
