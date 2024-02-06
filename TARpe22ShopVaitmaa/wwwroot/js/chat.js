a6190fef3cc76be08f0f9f5e821a088098dcaddc
Patrick <Patrick@DESKTOP-7BVVI1C>
2024-02-07 01:29:05 +0200

    a

--- TARpe22ShopVaitmaa/wwwroot/js/chat.js
+++ TARpe22ShopVaitmaa/wwwroot/js/chat.js
@@ -1,241 +1,30 @@
-﻿console.error('Error sending private message
-        self.joinRoom = function (room) {
-            connection.invoke("Join", room.name()).then(function () {
-                self.joinedRoom(room);
-                self.userList();
-                self.messageHistory();
-            });
-        }
+﻿"use strict";
 
-        self.roomList = function () {
-            fetch('/api/Rooms')
-                .then(response => response.json())
-                .then(data => {
-                    self.chatRooms.removeAll();
-                    for (var i = 0; i < data.length; i++) {
-                        self.chatRooms.push(new ChatRoom(data[i].id, data[i].name, data[i].admin));
-                    }
+var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
 
-                    if (self.chatRooms().length > 0)
-                        self.joinRoom(self.chatRooms()[0]);
-                });
-        }
+//Disable the send button until connection is established.
+document.getElementById("sendButton").disabled = true;
 
-        self.userList = function () {
-            connection.invoke("GetUsers", self.joinedRoom()?.name()).then(function (result) {
-                self.chatUsers.removeAll();
-                for (var i = 0; i < result.length; i++) {
-                    self.chatUsers.push(new ChatUser(result[i].userName,
-                        result[i].fullName,
-                        result[i].avatar,
-                        result[i].currentRoom,
-                        result[i].device))
-                }
-            });
-        }
+connection.on("ReceiveMessage", function (user, message) {
+    var li = document.createElement("li");
+    document.getElementById("messagesList").appendChild(li);
+    // We can assign user-supplied strings to an element's textContent because it
+    // is not interpreted as markup. If you're assigning in any other way, you 
+    // should be aware of possible script injection concerns.
+    li.textContent = `${user} says ${message}`;
+});
 
-        self.createRoom = function () {
-            var roomName = $("#roomName").val();
-            fetch('/api/Rooms', {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ name: roomName })
-            });
-        }
+connection.start().then(function () {
+    document.getElementById("sendButton").disabled = false;
+}).catch(function (err) {
+    return console.error(err.toString());
+});
 
-        self.editRoom = function () {
-            var roomId = self.joinedRoom().id();
-            var roomName = $("#newRoomName").val();
-            fetch('/api/Rooms/' + roomId, {
-                method: 'PUT',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ id: roomId, name: roomName })
-            });
-        }
-
-        self.deleteRoom = function () {
-            fetch('/api/Rooms/' + self.joinedRoom().id(), {
-                method: 'DELETE',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ id: self.joinedRoom().id() })
-            });
-        }
-
-        self.deleteMessage = function () {
-            var messageId = $("#itemToDelete").val();
-
-            fetch('/api/Messages/' + messageId, {
-                method: 'DELETE',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ id: messageId })
-            });
-        }
-
-        self.messageHistory = function () {
-            fetch('/api/Messages/Room/' + viewModel.joinedRoom().name())
-                .then(response => response.json())
-                .then(data => {
-                    self.chatMessages.removeAll();
-                    for (var i = 0; i < data.length; i++) {
-                        var isMine = data[i].fromUserName == self.myProfile().userName();
-                        self.chatMessages.push(new ChatMessage(data[i].id,
-                            data[i].content,
-                            data[i].timestamp,
-                            data[i].fromUserName,
-                            data[i].fromFullName,
-                            isMine,
-                            data[i].avatar))
-                    }
-
-                    $(".messages-container").animate({ scrollTop: $(".messages-container")[0].scrollHeight }, 1000);
-                });
-        }
-
-        self.roomAdded = function (room) {
-            self.chatRooms.push(room);
-        }
-
-        self.roomUpdated = function (updatedRoom) {
-            var room = ko.utils.arrayFirst(self.chatRooms(), function (item) {
-                return updatedRoom.id() == item.id();
-            });
-
-            room.name(updatedRoom.name());
-
-            if (self.joinedRoom().id() == room.id()) {
-                self.joinRoom(room);
-            }
-        }
-
-        self.roomDeleted = function (id) {
-            var temp;
-            ko.utils.arrayForEach(self.chatRooms(), function (room) {
-                if (room.id() == id)
-                    temp = room;
-            });
-            self.chatRooms.remove(temp);
-        }
-
-        self.messageDeleted = function (id) {
-            var temp;
-            ko.utils.arrayForEach(self.chatMessages(), function (message) {
-                if (message.id() == id)
-                    temp = message;
-            });
-            self.chatMessages.remove(temp);
-        }
-
-        self.userAdded = function (user) {
-            self.chatUsers.push(user);
-        }
-
-        self.userRemoved = function (id) {
-            var temp;
-            ko.utils.arrayForEach(self.chatUsers(), function (user) {
-                if (user.userName() == id)
-                    temp = user;
-            });
-            self.chatUsers.remove(temp);
-        }
-
-        self.uploadFiles = function () {
-            var form = document.getElementById("uploadForm");
-            $.ajax({
-                type: "POST",
-                url: '/api/Upload',
-                data: new FormData(form),
-                contentType: false,
-                processData: false,
-                success: function () {
-                    $("#UploadedFile").val("");
-                },
-                error: function (error) {
-                    alert('Error: ' + error.responseText);
-                }
-            });
-        }
-    }
-
-    function ChatRoom(id, name, admin) {
-        var self = this;
-        self.id = ko.observable(id);
-        self.name = ko.observable(name);
-        self.admin = ko.observable(admin);
-    }
-
-    function ChatUser(userName, fullName, avatar, currentRoom, device) {
-        var self = this;
-        self.userName = ko.observable(userName);
-        self.fullName = ko.observable(fullName);
-        self.avatar = ko.observable(avatar);
-        self.currentRoom = ko.observable(currentRoom);
-        self.device = ko.observable(device);
-    }
-
-    function ChatMessage(id, content, timestamp, fromUserName, fromFullName, isMine, avatar) {
-        var self = this;
-        self.id = ko.observable(id);
-        self.content = ko.observable(content);
-        self.timestamp = ko.observable(timestamp);
-        self.timestampRelative = ko.pureComputed(function () {
-            // Get diff
-            var date = new Date(self.timestamp());
-            var now = new Date();
-            var diff = Math.round((date.getTime() - now.getTime()) / (1000 * 3600 * 24));
-
-            // Format date
-            var { dateOnly, timeOnly } = formatDate(date);
-
-            // Generate relative datetime
-            if (diff == 0)
-                return `Today, ${timeOnly}`;
-            if (diff == -1)
-                return `Yestrday, ${timeOnly}`;
-
-            return `${dateOnly}`;
-        });
-        self.timestampFull = ko.pureComputed(function () {
-            var date = new Date(self.timestamp());
-            var { fullDateTime } = formatDate(date);
-            return fullDateTime;
-        });
-        self.fromUserName = ko.observable(fromUserName);
-        self.fromFullName = ko.observable(fromFullName);
-        self.isMine = ko.observable(isMine);
-        self.avatar = ko.observable(avatar);
-    }
-
-    function ProfileInfo(userName, fullName, avatar) {
-        var self = this;
-        self.userName = ko.observable(userName);
-        self.fullName = ko.observable(fullName);
-        self.avatar = ko.observable(avatar);
-    }
-
-    function formatDate(date) {
-        // Get fields
-        var year = date.getFullYear();
-        var month = date.getMonth() + 1;
-        var day = date.getDate();
-        var hours = date.getHours();
-        var minutes = date.getMinutes();
-        var d = hours >= 12 ? "PM" : "AM";
-
-        // Correction
-        if (hours > 12)
-            hours = hours % 12;
-
-        if (minutes < 10)
-            minutes = "0" + minutes;
-        
-        // Result
-        var dateOnly = `${day}/${month}/${year}`;
-        var timeOnly = `${hours}:${minutes} ${d}`;
-        var fullDateTime = `${dateOnly} ${timeOnly}`;
-
-        return { dateOnly, timeOnly, fullDateTime };
-    }
-
-    var viewModel = new AppViewModel();
-    ko.applyBindings(viewModel);
+document.getElementById("sendButton").addEventListener("click", function (event) {
+    var user = document.getElementById("userInput").value;
+    var message = document.getElementById("messageInput").value;
+    connection.invoke("SendMessage", user, message).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
 });
